63b0c58a69a206f85e583331a048353816894e04
3009044520101@ingenieria.usac.edu.gt <3009044520101@ingenieria.usac.edu.gt>
2022-03-11 21:26:26 -0600

    Se creó la interfaz gráfica

---
+++ interfaz.py
@@ -0,0 +1,75 @@
+import os
+import tkinter
+from tkinter import *
+from tkinter import ttk
+from tkinter import scrolledtext
+from tkinter import messagebox
+from tkinter.filedialog import askopenfilename
+
+
+class interfaz:
+    def __init__(self, ventana):
+        self.ventana = ventana
+        self.ventana.geometry (str(720) + 'x' + str(720) + '+' + str(self.ventana.winfo_screenwidth() // 2 - 720 // 2) + '+' + str(self.ventana.winfo_screenheight() // 2 - 720 // 2))
+        self.ventana.resizable (0, 0)
+        self.textobox = scrolledtext.ScrolledText(self.ventana, width = 85, height = 38)
+        self.textobox.place(x = 5, y = 90)
+        self.boton = Button (self.ventana, text='Cargar Archivo', command=self.abrirarchivo)
+        self.boton.place(x = 15, y = 25)
+        self.boton2 = Button (self.ventana, text='Analizar', command=self.anarchivo)
+        self.boton2.place(x = 110, y = 25)
+        n = tkinter.StringVar()
+        self.combox = ttk.Combobox(self.ventana, state= 'readonly', textvariable=n)
+        lista = ['Reporte de tokens', 'Reporte de errores', 'Manual de usuario', 'Manual técnico']
+        self.combox ['values'] = lista
+        self.combox.set('Reportes')
+        self.combox.bind('<<ComboboxSelected>>', self.seleccionescombo)
+        self.combox.place(x= 200, y = 25)
+        self.ventana.mainloop ()
+
+    def abrirarchivo(self):
+        try:
+            archivo = askopenfilename(filetypes= [('Archivos Forms', '*.form')])
+            seleccion = open(archivo, encoding= 'utf-8')
+            contentfile = seleccion.read()
+            seleccion.close()
+            self.textobox.delete('1.0', END)
+            self.textobox.insert(tkinter.INSERT, contentfile)
+        except IOError:
+            messagebox.showwarning('Proyecto 1', 'Error al cargar el archivo')
+
+
+    def anarchivo(self):
+        contenido = self.textobox.get('1.0', END)
+        if len(contenido) -1>0:
+            print(contenido)
+        else:
+            messagebox.showwarning('Proyecto 1', 'Error: No hay contenido en el archivo')
+
+    def seleccionescombo(self, eventObject):
+        if self.combox.get() == 'Reporte de tokens':
+            print('uno')
+        elif self.combox.get() == 'Reporte de errores':
+            print('dos')
+        elif self.combox.get() == 'Manual de usuario':
+            #os.system('Manual de usuario.pdf')
+            print('tres')
+        elif self.combox.get() == 'Manual técnico':
+            #os.system('Manual técnico.pdf')
+            print('cuatro')
+     
+
+if __name__ == '__main__':
+    envio = Tk()
+    interfaz(envio)
+
+
+
+
+
+
+
+
+
+
+
